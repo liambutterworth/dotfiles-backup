db012e27c5820f255023ab1bca47db1e576eb9c9
Liam Butterworth <liambutterworth@protonmail.com>
2021-07-16 11:50:16 -0500

    update fish exports

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -85,12 +85,9 @@ set -gx CARGO_HOME "$XDG_DATA_HOME/cargo"
 set -gx RUSTUP_HOME "$XDG_DATA_HOME/rustup"
 set -gx PYENV_ROOT "$XDG_DATA_HOME/pyenv"
 set -gx FZF_DEFAULT_COMMAND "rg --files --hidden"
-set -gx FZF_FIND_FILE_COMMAND $FZF_DEFAULT_COMMAND
 set -gx FZF_CTRL_T_COMMAND "rg --files --hidden"
 set -gx FZF_DEFAULT_OPTS "--color $FZF_COLORS --no-preview"
-set -gx FZF_TMUX 1
-
-eval (dircolors -c $XDG_CONFIG_HOME/dircolors)
+set -gx FZF_FIND_FILE_COMMAND $FZF_DEFAULT_COMMAND
 
 if test -e $XDG_CONFIG_HOME/fish/profile.fish
     source $XDG_CONFIG_HOME/fish/profile.fish
