9fa7167c444931b2f425f5c9e86381cc385ef975
Liam Butterworth <liambutterworth@protonmail.com>
2021-07-16 11:21:37 -0500

    add awesome wm

---
+++ .config/awesome/rc.lua
@@ -0,0 +1,19 @@
+pcall(require, 'luarocks.loader')
+
+local awful = require('awful')
+local beautiful = require('beautiful')
+local naughty = require('naughty')
+
+awful.util.shell = 'fish'
+terminal = 'alacritty'
+editor = 'nvim'
+
+beautiful.init(require('theme'))
+root.keys(require('keys'))
+
+require('errors')
+require('interface')
+
+awful.spawn.with_shell('~/.config/awesome/scripts/autorun.sh')
+
+require('awful.autofocus') -- TODO remove in v5
