08c9f1f8cb2ce22ba930b0f660d028eefc12f32e
Liam Butterworth <liambutterworth@randallreilly.com>
2021-08-07 18:34:25 -0500

    cleanup autopairs config

--- .config/nvim/lua/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,14 +1,14 @@
 local autopairs = require('nvim-autopairs')
-local compe = require('nvim-autopairs.completion.compe')
-local lua = require('nvim-autopairs.rules.endwise-lua')
+local autopairs_compe = require('nvim-autopairs.completion.compe')
+local autopairs_lua = require('nvim-autopairs.rules.endwise-lua')
 
 autopairs.setup({
     check_ts = true,
 })
 
-autopairs.add_rules(lua)
-
-compe.setup({
+autopairs_compe.setup({
     map_cr = true,
     map_complete = true,
 })
+
+autopairs.add_rules(autopairs_lua)
