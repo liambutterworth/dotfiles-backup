4e2823ebe74c1828c01f1a9d36374544d1d4f104
Liam Butterworth <liambutterworth@protonmail.com>
2021-08-18 21:35:46 -0500

    add ES command

--- .config/nvim/lua/globals/commands.lua
+++ .config/nvim/lua/globals/commands.lua
@@ -1,9 +1,16 @@
+local query = require('support.query')
+
 function QueryConfig()
     query:open_config()
+end
+
+function ES(name)
+    query:es(name)
 end
 
 vim.cmd([[
 
 command! QueryConfig call v:lua.QueryConfig()
+command! -nargs=1 ES call v:lua.ES(<f-args>)
 
 ]])
--- .config/nvim/lua/support/query.lua
+++ .config/nvim/lua/support/query.lua
@@ -22,4 +22,44 @@ function query:get_config()
     return self.config
 end
 
+function query:get_buffer_string()
+    local lines = vim.fn.getline(1, '$')
+    local string = vim.fn.join(lines)
+
+    return vim.fn.substitute(string, ' ', '', 'g')
+end
+
+function query:curl(options)
+    local command = { 'curl' }
+
+    if options.url then
+        table.insert(command, options.url)
+    end
+
+    for key, value in pairs(options.headers or {}) do
+        table.insert(command, "-H '" .. key .. ": " .. value .. "'")
+    end
+
+    if options.data then
+        table.insert(command, "-d '" .. options.data .. "'")
+    end
+
+    return vim.fn.system(table.concat(command, ' '))
+end
+
+function query:es(name)
+    local config = self:get_config().es[name]
+
+    local results = self:curl({
+        url = config.host .. '/' .. config.index .. '/_search?pretty',
+        data = self:get_buffer_string(),
+
+        headers = {
+            ['Content-Type'] = 'application/json'
+        }
+    })
+
+    print(results)
+end
+
 return query
