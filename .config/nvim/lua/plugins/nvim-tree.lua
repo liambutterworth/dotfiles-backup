c14207fa9f5b8cd7ca07267c8f3a4b3d7d6f982e
Liam Butterworth <liambutterworth@randallreilly.com>
2021-11-17 17:47:21 -0600

    change nvim tree mapping

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,18 +1,8 @@
 return function()
     local tree = require('nvim-tree')
-    local callback = require('nvim-tree.config').nvim_tree_callback
 
-    tree.setup({
-        view = {
-            mappings = {
-                list = {
-                    { key = { 'o' }, cb = callback('edit') },
-                    { key = { '<cr>' }, cb = callback('close') }
-                }
-            }
-        }
-    })
+    tree.setup()
 
-    vim.api.nvim_set_keymap('n', '<cr>', '<cmd>NvimTreeToggle<cr>', { noremap = true })
-    vim.api.nvim_set_keymap('n', '\\', '<cmd>NvimTreeFindFile<cr>', { noremap = true })
+    vim.api.nvim_set_keymap('n', '\\', '<cmd>NvimTreeToggle<cr>', { noremap = true })
+    vim.api.nvim_set_keymap('n', '|', '<cmd>NvimTreeFindFile<cr>', { noremap = true })
 end
