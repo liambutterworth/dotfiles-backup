4b1dde41a7b621ed3a1dadfbeecca8f46d27c43b
Liam Butterworth <liambutterworth@randallreilly.com>
2022-03-20 20:12:59 -0500

    remove awesome config

--- .config/awesome/interface/layouts.lua
+++
@@ -1,9 +0,0 @@
-local awful = require('awful')
-
-return {
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.top,
-    -- awful.layout.suit.floating,
-    awful.layout.suit.max.fullscreen,
-}
