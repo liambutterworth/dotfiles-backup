3d6228763adaf92b1d226baed7e5768ac780137f
Liam Butterworth <contact@liambutterworth.me>
2023-03-06 07:51:56 -0600

    remove fish profile and update gitignore

--- config/fish/profile.fish
+++
@@ -1,4 +0,0 @@
-set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set -x MAPBOX_ACCESS_TOKEN sk.eyJ1IjoibGlhbWJ1dHRlcndvcnRoIiwiYSI6ImNsNzZncG9oZzF6c2Yzbm50NDllNWtkbG8ifQ.EwzVMDxkdty--eO-XyJB8g
-
-eval (dircolors -c $HOME/.config/dircolors/dircolors)
